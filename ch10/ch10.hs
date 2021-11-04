9cab5081d3cd809dc413dd970cb299b59a573f42
Alex Chavkin <alexlchavkin@gmail.com>
2021-11-04 21:35:38 +0000

    Push ch10 stuff to gh for checkpoint

---
+++ ch10/ch10.hs
@@ -0,0 +1,22 @@
+-- Exercises Understanding Folds pg 365-367
+
+-- 1 b and c
+-- 2 
+-- NOTE: I did this for [1..5] instead of [1..3] by accident but same idea just longer and more annoying
+-- foldl (flip (*)) 1 [1..5]
+-- = foldl (flip (*)) (1*1) [2..5]
+-- = foldl (flip (*)) (2*(1*1)) [3..5]
+-- = foldl (flip (*)) (3*(2*(1*1))) [4..5]
+-- = foldl (flip (*)) (4*(3*(2*(1*1)))) [5]
+-- = foldl (flip (*)) (5*(4*(3*(2*(1*1))))) []
+-- = (5*(4*(3*(2*(1*1)))))
+-- = (5*(4*(3*(2*1))))
+-- = (5*(4*(3*2)))
+-- = (5*(4*6))
+-- = (5*24)
+-- = 120
+--
+-- 3 c
+-- 4 a
+-- 5 foldr (++) [] ["woot", "WOOT", "woot"] -- no base
+-- 6 
