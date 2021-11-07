687b502173a63b647469cb0519e61f85abd1ed8a
Alex Chavkin <alexlchavkin@gmail.com>
2021-11-07 15:24:27 -0500

    Finish ch10

--- ch10/ch10.hs
+++ ch10/ch10.hs
@@ -1,4 +1,4 @@
-1-- Exercises Understanding Folds pg 365-367
+-- Exercises Understanding Folds pg 365-367
 
 -- 1 b and c
 -- 2 
@@ -28,4 +28,146 @@
 -- 5h foldl const 0 "burritos"
 -- 5i foldl const 'z' [1..5]
 
+-- Exercises Scans pg 379
 
+fibsN :: Int -> Integer
+fibsN x = fibs !! x
+--1
+fibs :: [Integer]
+fibs = take 20 $ 1 : scanl (+) 1 fibs
+
+--2 
+fibsLessThan100 :: [Integer]
+fibsLessThan100 = filter (<100) fibs
+
+--3
+
+factorials :: [Integer]
+factorials = scanl (*) 1 [2..]
+
+factorial :: Int -> Integer
+factorial x = factorials !! x
+
+-- Chapter Exercises
+
+-- Warm up and review pg 379-380
+
+stops = "pbtdkg"
+vowels = "aeiou"
+
+--1a
+
+threeTups :: [(Char,Char,Char)]
+threeTups = [(a, b, c) | a <- stops, b <- vowels, c <- stops]
+
+--1b
+
+threeTups' :: [(Char,Char,Char)]
+threeTups' = [(a, b, c) | a <- stops, b <- vowels, c <- stops, a == 'p']
+
+--1c
+
+nouns = "Actor  Gold    Painting \
+         \ Advertisement   Grass   Parrot \
+         \ Afternoon   Greece  Pencil \
+         \ Airport     Guitar  Piano \
+         \ Ambulance   Hair    Pillow" -- credit eslgrammar.com
+
+verbs = "Accept  Guess \
+        \ Achieve     Harass \
+        \ Add     Hate \
+        \ Admire  Hear \
+        \ Admit   Help \ 
+        \ Adopt   Hit \
+        \ Advise  Hope" -- credit eslgrammar.com
+
+myStrings :: Char -> String -> [String] -- I wrote in last chapter exercises
+myStrings _ [] = []
+myStrings sep s
+  | length y == 0 = takeWhile (/= sep) s : (myStrings sep y)
+  | otherwise = takeWhile (/= sep) s : (myStrings sep $ tail y)
+  where y = dropWhile (/= sep) s
+
+myWords :: String -> [String]
+myWords s = myStrings ' ' s
+
+sentenceTups :: [(String,String,String)]
+sentenceTups = [(a,b,c) | a <- myWords nouns, b <- myWords verbs, 
+                                              c <- myWords nouns]
+
+-- 2
+
+-- get avg number of characters in a group of words like a sentence
+seekritFunc :: String -> Int
+seekritFunc x = div (sum (map length (words x))) (length (words x))
+
+--3
+
+seekritFunc' :: Fractional a => String -> a
+seekritFunc' x = (/) (fromIntegral . sum . map length . words $ x) 
+                      (fromIntegral . length . words $ x)
+
+-- Rewriting Functions Using Folds pg 381-384
+
+-- I don't think some of these can get more pointfree without using
+-- a pointless substitution like a go-function just using a lambda
+-- to move the arguments to the right side of the definition. Maybe
+-- I'm wrong though.
+
+--1 
+
+myOr :: [Bool] -> Bool
+myOr = foldr (||) False
+
+--2 
+
+myAny :: (a -> Bool) -> [a] -> Bool
+myAny f = foldr ((||) . f) False
+
+--3 
+
+myElem :: Eq a => a -> [a] -> Bool
+myElem i = foldr ((||) . (== i)) False
+
+myElem' :: Eq a => a -> [a] -> Bool
+myElem' i = myAny (== i)
+
+--4
+
+myReverse :: [a] -> [a]
+myReverse = foldl (flip (:)) []
+
+--5
+
+myMap :: (a -> b) -> [a] -> [b]
+myMap f = foldr ((:) . f) []
+
+--6
+
+myFilter :: (a -> Bool) -> [a] -> [a]
+myFilter f = foldr ((++) . (\x -> if f x then [x] else [])) []
+
+--7
+
+squish :: [[a]] -> [a]
+squish = foldr (++) []
+
+--8
+
+squishMap :: (a -> [b]) -> [a] -> [b]
+squishMap f = foldr ((++) . f) []
+
+--9
+
+squishAgain :: [[a]] -> [a]
+squishAgain = squishMap id
+
+--10
+
+myMaximumBy :: (a -> a -> Ordering) -> [a] -> a
+myMaximumBy f xs = foldr (\x y -> if f x y == GT then x else y) (head xs) xs
+
+--11
+
+myMinimumBy :: (a -> a -> Ordering) -> [a] -> a
+myMinimumBy f xs = foldr (\x y -> if f x y == LT then x else y) (head xs) xs
