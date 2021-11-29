a918819755151aa5f0fb0d7b8e88a32c26dd6109
Alex Chavkin <alexlchavkin@gmail.com>
2021-11-29 04:11:10 +0000

    add ch13

---
+++ ch13/ch13.hs
@@ -0,0 +1,8 @@
+-- Intermission Check Your Understanding pg 506
+
+--1  forever, when
+--2  Data.Bits
+--3  types that blacktip defines
+--4a Control.Concurrent.MVar, FilePath.System.CurrentOS, Control.Concurrent
+--4b Filesystem.writeFile
+--4c Control.Monad
