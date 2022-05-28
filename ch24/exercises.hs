737a07ba63303a7f10cf78973c64cfcceecb7ef9
Alex Chavkin <alexlchavkin@gmail.com>
2022-05-28 21:12:25 +0000

    more ch24 exercises, phone no parser

--- ch24/exercises.hs
+++ ch24/exercises.hs
@@ -18,12 +18,38 @@ base10Integer = read <$> some parseDigit
 
 base10Integer' :: Parser Integer
 base10Integer' = do
-  m <- many (oneOf "-")
+  m <- many (char '-')
   let b = base10Integer
   if null m then b else liftA2 (*) (pure (-1)) b
 
 --4
---TODO
+type NumberingPlanArea = Int
+type Exchange = Int
+type LineNumber = Int
+
+data PhoneNumber = PhoneNumber NumberingPlanArea Exchange LineNumber deriving (Eq, Show)
+
+parseNDigit :: Int -> Parser Int
+parseNDigit n = do
+  m <- many $ char '('
+  x <- read <$> count n parseDigit
+  case null m of
+    True -> return x
+    False -> (char ')') >>= \_ -> return x
+
+parsePhone :: Parser PhoneNumber
+parsePhone = do
+  optional $ string "1-" <|> string "1 "
+  npa <- parseNDigit 3
+  optional $ oneOf "- "
+  exchange <- parseNDigit 3
+  optional $ oneOf "- "
+  line <- parseNDigit 4
+  return $ PhoneNumber npa exchange line
+    
+    
+
+    
 
 --5
 --TODO
