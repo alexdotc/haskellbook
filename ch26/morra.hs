ee3746f9bfebc8a7f4c39a8aa9a7fdc7cbd2e35b
Alex Chavkin <alexlchavkin@gmail.com>
2022-09-18 21:45:23 -0400

    ch26: finish morra game pt1

--- ch26/morra.hs
+++ ch26/morra.hs
@@ -1,7 +1,10 @@
 module Main where
 
+import Control.Monad (forever)
 import Control.Monad.IO.Class
 import Control.Monad.Trans.State
+import System.Exit (exitSuccess)
+import System.IO
 import System.Random (randomRIO)
 
 type Morra = StateT (PS,CS) IO PlayerWon
@@ -20,9 +23,36 @@ morra n = do
 playMorra :: Int -> IO PlayerWon
 playMorra n = do
   m <- randomRIO (1, 10)
+  putStrLn $ "You guessed: " ++ show n
+  putStrLn $ "Computer guessed: " ++ show m
   return $ (n+m) `mod` 2 == 1
+
+pprintScores :: (PS,CS) -> IO ()
+pprintScores (ps,cs) = putStrLn $ "Your score: " ++ show ps 
+    ++ "\tComputer score: " ++ show cs ++ "\n"
+
+finalScores :: (PS,CS) -> IO ()
+finalScores (ps,cs)
+  | ps > cs = putStrLn "Congratulations! You won!"
+  | ps < cs = putStrLn "The computer won! You failed!"
+  | otherwise = putStrLn "This game of Morra is tied!"
+
+morraLoop :: (PS,CS) -> IO ()
+morraLoop s = do
+    putStr "Guess a number between 1 and 10 ('q' to quit): " 
+    n <- hGetLine stdin
+    case n of
+      "q" -> do finalScores s; putStrLn ""; exitSuccess
+      _   -> return ()
+    (result, scores) <- runStateT (morra $ read n) s
+    case result of
+      True -> putStrLn "You win this round!"
+      False -> putStrLn "You lose this round!"
+    pprintScores scores
+    morraLoop scores
 
 main :: IO ()
 main = do
-  scores <- execStateT (morra 3) (0,0)
-  print scores
+  hSetBuffering stdout NoBuffering
+  putStrLn "Welcome to Morra! You are odds, computer is evens. FIGHT!"
+  morraLoop (0,0)
